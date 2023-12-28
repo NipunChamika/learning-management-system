98e004cd0c405e8fb9492690a7dcbaf4d819dd32
Nipun Chamika <nipunchamika11@gmail.com>
2023-12-28 19:21:52 +0530

    feat: Generate CreateStudentsTable migration file

---
+++ backend/src/typeorm/migrations/1703769340585-CreateStudentsTable.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateStudentsTable1703769340585 implements MigrationInterface {
+    name = 'CreateStudentsTable1703769340585'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`students\` (\`studentId\` int NOT NULL AUTO_INCREMENT, \`programEnrolled\` varchar(255) NOT NULL, \`passedAL\` tinyint NOT NULL, \`userId\` int NULL, UNIQUE INDEX \`REL_e0208b4f964e609959aff431bf\` (\`userId\`), PRIMARY KEY (\`studentId\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`students\` ADD CONSTRAINT \`FK_e0208b4f964e609959aff431bf9\` FOREIGN KEY (\`userId\`) REFERENCES \`users\`(\`userId\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`students\` DROP FOREIGN KEY \`FK_e0208b4f964e609959aff431bf9\``);
+        await queryRunner.query(`DROP INDEX \`REL_e0208b4f964e609959aff431bf\` ON \`students\``);
+        await queryRunner.query(`DROP TABLE \`students\``);
+    }
+
+}
