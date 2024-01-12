3429622f029f190fb579ea43bb2e9ae1667cb6c8
Nipun Chamika <nipunchamika11@gmail.com>
2024-01-12 18:24:59 +0530

    feat: Add Login Component

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,25 @@
+import { Card } from "primereact/card";
+import { InputText } from "primereact/inputtext";
+
 const Login = () => {
   return (
     <>
-      <div>Login</div>
+      <div>
+        <Card
+          title="Login"
+          className="min-w-[440px]"
+          style={{ width: "440px", padding: "3px" }}
+        >
+          <form>
+            <div className="flex flex-col">
+              <label htmlFor="email">Email</label>
+              <InputText id="email" />
+              <label htmlFor="password">Password</label>
+              <InputText id="password" />
+            </div>
+          </form>
+        </Card>
+      </div>
     </>
   );
 };
