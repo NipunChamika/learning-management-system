dc1d53307add81deae03a4012e5c29c2a5617755
Nipun Chamika <nipunchamika11@gmail.com>
2024-01-16 19:55:29 +0530

    feat: Enable CORS to Allow Requests from Frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS to allow requests from frontend
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
