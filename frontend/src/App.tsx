0ee30f84c365fe47f5b8c8f79bcf62230d52346f
Nipun Chamika <nipunchamika11@gmail.com>
2024-01-17 19:00:19 +0530

    feat: Implement Context API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import { UserContext } from "./context/UserContext";
+import { useState } from "react";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/login", element: <Login /> },
+  ]);
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
   return (
-    <>
-      <Login />
-    </>
+    <UserContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      <RouterProvider router={router} />
+    </UserContext.Provider>
   );
 }
 
---
+++ frontend/src/context/UserContext.ts
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react";
+
+interface UserContextInterface {
+  isLoggedIn: boolean;
+  setLoggedIn: (isLoggedIn: boolean) => void;
+}
+
+export const UserContext = createContext<UserContextInterface | undefined>(
+  undefined
+);
+
+export function useUserContext() {
+  const userContext = useContext(UserContext);
+
+  if (userContext === undefined) {
+    throw new Error("useUserContext must be used with a UserContext");
+  }
+
+  return userContext;
+}
