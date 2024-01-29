b8bed7d66344ee8bbf91639dc6477e4ec1486b5a
Nipun Chamika <nipunchamika11@gmail.com>
2024-01-29 14:24:38 +0530

    feat: Upgrade NavBar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
-import { Box, IconButton, Image } from "@chakra-ui/react";
-import logo from "./../../assets/logo.png";
-import profile from "./../../assets/profile.png";
-import { SettingsIcon } from "@chakra-ui/icons";
+import { Box, IconButton, Image, Text } from "@chakra-ui/react";
+import logo from "./../../assets/logo.svg";
+import dark from "../../assets/dark.svg";
+import settings from "../../assets/settings.svg";
 
 const NavBar = () => {
   return (
@@ -11,30 +11,40 @@ const NavBar = () => {
         display="flex"
         justifyContent="space-between"
         alignItems="center"
-        px="6"
+        px="40px"
+        py="24px"
+        borderBottom="1px"
+        borderColor="#F4F4F4"
       >
-        <Image src={logo} alt="Logo" boxSize="80px" />
-        <Box display="flex" alignItems="center" justifyContent="center" gap="4">
-          <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            gap="2"
-          >
-            <Image src={profile} alt="Profile" boxSize="40px" />
-            <Box>Nipun</Box>
+        <Box display="flex" alignItems="center" gap="10px">
+          <Image src={logo} alt="Logo" h="38px" w="50px" />
+          <Text fontSize="30px" fontWeight="500">
+            La
+            <Box as="span" color="#317CEC">
+              mko
+            </Box>
+          </Text>
+        </Box>
+        <Box display="flex" alignItems="center" gap="24px">
+          <Box>
+            <IconButton
+              icon={<Image src={dark} alt="Dark Mode" />}
+              aria-label="Dark Mode"
+              variant="ghost"
+              isRound={true}
+              _hover={{ bgColor: "#f9f6fd" }}
+            />
+            {/* <Image src={dark} alt="Logo" w="26px" h="26px" /> */}
           </Box>
           <Box>
             <IconButton
-              icon={<SettingsIcon />}
+              icon={<Image src={settings} alt="Settings" />}
               aria-label="Settings"
+              variant="ghost"
               isRound={true}
-              fontSize="20"
-              boxSize="40px"
-              variant="none"
-              color="#757575"
               _hover={{ bgColor: "#f9f6fd" }}
             />
+            {/* <Image src={settings} alt="Logo" w="26px" h="26px" /> */}
           </Box>
         </Box>
       </Box>
