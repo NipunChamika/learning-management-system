75a09a2e44654d6ed2d361f8efb08335e6874768
Nipun Chamika <nipunchamika11@gmail.com>
2024-02-01 15:51:45 +0530

    feat: Implement design system with Chakra UI

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -4,6 +4,81 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const tokens = {
+  colors: {
+    light: {
+      "bg-default": "#F4F7FE",
+      "bg-color": "white",
+      "border-color": "#F4F4F4",
+      "text-color": "#1A2744",
+      "low-text-color": "#978D8D",
+      "menu-hover-bg": "#317CEC",
+      "menu-active-bg": "#317CEC",
+      "card-bg": "#FAFAFA",
+    },
+    dark: {
+      "bg-default": "#232323",
+      "bg-color": "#282829",
+      "border-color": "#373434",
+      "text-color": "white",
+      "low-text-color": "#978D8D",
+      "menu-hover-bg": "#317CEC",
+      "menu-active-bg": "#317CEC",
+      "card-bg": "#232323",
+    },
+  },
+};
+
+const semanticTokens = {
+  colors: {
+    "bg-default": {
+      default: tokens.colors.light["bg-default"],
+      _dark: tokens.colors.dark["bg-default"],
+    },
+    "bg-color": {
+      default: tokens.colors.light["bg-color"],
+      _dark: tokens.colors.dark["bg-color"],
+    },
+    "border-color": {
+      default: tokens.colors.light["border-color"],
+      _dark: tokens.colors.dark["border-color"],
+    },
+    "text-color": {
+      default: tokens.colors.light["text-color"],
+      _dark: tokens.colors.dark["text-color"],
+    },
+    "low-text-color": {
+      default: tokens.colors.light["low-text-color"],
+      _dark: tokens.colors.dark["low-text-color"],
+    },
+    "menu-hover-bg": {
+      default: tokens.colors.light["menu-hover-bg"],
+      _dark: tokens.colors.dark["menu-hover-bg"],
+    },
+    "menu-active-bg": {
+      default: tokens.colors.light["menu-active-bg"],
+      _dark: tokens.colors.dark["menu-active-bg"],
+    },
+    "card-bg": {
+      default: tokens.colors.light["card-bg"],
+      _dark: tokens.colors.dark["card-bg"],
+    },
+  },
+};
+
+const styles = {
+  global: {
+    body: {
+      background: "bg-default",
+    },
+  },
+};
+
+// black: "#1A2744",
+//       white: "#FFFFFF",
+//       border: "#F4F4F4",
+//       primary: "#317CEC",
+
+const theme = extendTheme({ config, styles, semanticTokens });
 
 export default theme;
