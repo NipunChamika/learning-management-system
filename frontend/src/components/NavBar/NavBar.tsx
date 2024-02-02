d20964af0d4baf04113df388fcf9fc2028e9593e
Nipun Chamika <nipunchamika11@gmail.com>
2024-02-02 15:23:22 +0530

    feat: Upgrade NavBar to use icon components

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,19 +1,17 @@
-import { Box, IconButton, Image, Text, useColorMode } from "@chakra-ui/react";
-import logo from "./../../assets/logo.svg";
-import darkIcon from "../../assets/dark.svg";
-import lightIcon from "../../assets/light.svg";
-import settings from "../../assets/settings.svg";
-import menu from "../../assets/menu.svg";
+import { Box, Flex, IconButton, Text, useColorMode } from "@chakra-ui/react";
+import { DarkIcon } from "../../icons/DarkIcon";
+import { LightIcon } from "../../icons/LightIcon";
+import { SettingsIcon } from "../../icons/SettingsIcon";
+import { Logo } from "../../icons/Logo";
 
 const NavBar = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
     <>
-      <Box
+      <Flex
         // bg="coral"
         bg="bg-color"
-        display="flex"
         justifyContent="space-between"
         alignItems="center"
         px="40px"
@@ -21,58 +19,35 @@ const NavBar = () => {
         borderBottom="1px"
         borderColor="border-color"
       >
-        <Box display="flex" alignItems="center" gap="10px">
-          <Image src={logo} alt="Logo" h="38px" w="50px" />
-          <Text fontSize="30px" fontWeight="500">
+        <Flex alignItems="center" gap="10px">
+          <Logo h="38px" w="50px" color="bg-color" />
+          <Text fontSize="30px" fontWeight="500" color="text-color">
             La
             <Box as="span" color="#317CEC">
               mko
             </Box>
           </Text>
-        </Box>
-        <Box display="flex" alignItems="center" gap="24px">
-          {/* <Box>
-            <IconButton
-              icon={<Image src={menu} alt="Dark Mode" />}
-              aria-label="Dark Mode"
-              variant="ghost"
-              isRound={true}
-              _hover={{ bgColor: "#f9f6fd" }}
-              w="26px"
-              h="26px"
-              onClick={toggleColorMode}
-            /> */}
-          {/* <Image src={dark} alt="Logo" w="26px" h="26px" /> */}
-          {/* </Box> */}
-
-          <Box>
-            <IconButton
-              icon={
-                <Image
-                  src={colorMode === "light" ? darkIcon : lightIcon}
-                  alt="Dark Mode"
-                />
-              }
-              aria-label="Dark Mode"
-              variant="ghost"
-              isRound={true}
-              _hover={{ bgColor: "#f9f6fd" }}
-              onClick={toggleColorMode}
-            />
-            {/* <Image src={dark} alt="Logo" w="26px" h="26px" /> */}
-          </Box>
-          <Box>
-            <IconButton
-              icon={<Image src={settings} alt="Settings" />}
-              aria-label="Settings"
-              variant="ghost"
-              isRound={true}
-              _hover={{ bgColor: "#f9f6fd" }}
-            />
-            {/* <Image src={settings} alt="Logo" w="26px" h="26px" /> */}
-          </Box>
-        </Box>
-      </Box>
+        </Flex>
+        <Flex alignItems="center" gap="24px">
+          <IconButton
+            variant="ghost"
+            isRound={true}
+            _hover={{ bgColor: "#f9f6fd" }}
+            aria-label="Color Mode"
+            fontSize="26"
+            onClick={toggleColorMode}
+            icon={colorMode === "light" ? <DarkIcon /> : <LightIcon />}
+          />
+          <IconButton
+            variant="ghost"
+            isRound={true}
+            _hover={{ bgColor: "#f9f6fd" }}
+            aria-label="Settings"
+            fontSize="26"
+            icon={<SettingsIcon />}
+          />
+        </Flex>
+      </Flex>
     </>
   );
 };
