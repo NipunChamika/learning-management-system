30c0b3b4adcad419007819200547a00b43412585
Nipun Chamika <nipunchamika11@gmail.com>
2024-02-05 20:05:38 +0530

    feat: Dynamically render CardItems based on courses data from backend

--- frontend/src/pages/Courses/Courses.tsx
+++ frontend/src/pages/Courses/Courses.tsx
@@ -1,13 +1,74 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import CardItem from "../../components/CardItem/CardItem";
+import Course1 from "../../assets/Program3.png";
+
+interface Course {
+  courseId: number;
+  courseName: string;
+  courseCode: string;
+}
+
+interface ResponseData {
+  courses: Course[];
+}
 
 const Courses = () => {
+  const [courses, setCourses] = useState<Course[]>([]);
+  const location = useLocation();
+
+  const { programId } = location.state;
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInJvbGUiOiJBRE1JTiIsImlhdCI6MTcwNzE0Mjk0MSwiZXhwIjoxNzA3MTQ2NTQxfQ.j4WmwmhgFVVm2s_Bt5QSzbb89gWDJwZKx69YZLGl_64",
+      },
+    };
+
+    axios
+      .get<ResponseData>(
+        "http://localhost:3000/course/program/" + programId,
+        config
+      )
+      .then((res) => {
+        console.log(res.data);
+        setCourses(res.data.courses);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleNavigate = () => {
+    // TODO: Navigate to Course Page with courseId
+  };
+
   return (
-    <Box>
-      <Text fontSize="30px" fontWeight="500" color="text-color">
-        Courses
-      </Text>
-      <Box width="100%" h="1px" bgColor="border-color" my="16px" />
-    </Box>
+    <>
+      <Box>
+        <Text fontSize="30px" fontWeight="500" color="text-color">
+          Courses
+        </Text>
+        <Box width="100%" h="1px" bgColor="border-color" my="16px" />
+      </Box>
+
+      <SimpleGrid columns={3} spacing={30} minChildWidth="275px">
+        {courses.map((course) => (
+          <CardItem
+            key={course.courseId}
+            src={Course1}
+            alt={`Course ${course.courseId}`}
+            code={course.courseCode}
+            name={course.courseName}
+            navigateTo={() => handleNavigate()}
+          />
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
 
